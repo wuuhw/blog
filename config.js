5b7479a14042a6de013f8bc1aee4b20dfcf31d21
wuhongwei <13167555404@163.com>
2019-07-10 15:15:36 +0800

    init paroject

---
+++ config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  url: 'https://wuhw.github.io',
+  pathPrefix: '/',
+  title: '梦里仗剑天涯的个人博客',
+  subtitle: '梦里仗剑天涯，<br/> 醒时码海无边。',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 5,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: '所有文章',
+      path: '/'
+    },
+    {
+      label: '关于自己',
+      path: '/pages/about'
+    },
+    {
+      label: '联系我',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'wuhw',
+    photo: '/photo.jpg',
+    bio: '梦里仗剑天涯，\n 醒时码海无边。',
+    contacts: {
+      email: 'wuHongwei586@gmail.com',
+      telegram: '#',
+      twitter: '#',
+      github: 'wuuhw',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
---
+++ src/constants/pagination.js
@@ -0,0 +1,7 @@
+// @flow
+const PAGINATION = {
+  PREV_PAGE: '← 上一页',
+  NEXT_PAGE: '→ 下一页'
+};
+
+export default PAGINATION;
